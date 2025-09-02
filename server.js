363a5b786075c955ae9ccd78bbe58aa6fd19fa19
johnlatif16 <johnlatif37@gmail.com>
2025-09-02 22:54:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ db.serialize(() => {
 });
 
 // تكوين nodemailer لإرسال البريد الإلكتروني
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
