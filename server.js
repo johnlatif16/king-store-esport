f6e230f2a9f63909087438351afae2286d4ecf21
johnlatif16 <johnlatif37@gmail.com>
2025-09-02 17:13:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// FileName: /server.js
-// FileContents: const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const session = require("express-session");
